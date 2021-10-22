92dfab305e04f40267f750431b8ed45266e6572e
MWatty <maura1984@icloud.com>
2021-10-22 22:36:09 +0000

    indentation amendments made to webhook pages

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -112,8 +112,6 @@ class StripeWH_Handler:
             status=200)
 
 
-
-
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
@@ -121,4 +119,3 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-            
