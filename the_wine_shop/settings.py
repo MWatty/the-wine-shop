40e9e480a019ed9b257f149e3211cad4466ba0b6
MWatty <maura1984@icloud.com>
2021-10-25 10:25:18 +0000

    Removed secret key and set debug

--- the_wine_shop/settings.py
+++ the_wine_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0klmb&+)g5sa82^98m&9b%0q3kax#2a)(%5w8(z&wn^%x5h970'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mw-the-wine-shop.herokuapp.com', 'localhost']
 
