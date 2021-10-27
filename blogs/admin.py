4f1d36e0d76a691c135f45d75e5444bada1e500a
MWatty <maura1984@icloud.com>
2021-10-27 20:05:21 +0000

    Added main blog template, views, forms and urls

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Blog, Comment
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'image_url',
+        'datetime',
+    )
+
+    ordering = ('name',) # to do a reverse ordering just stick a minus 
+                         # infront of the word 'name'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'blog',
+        'user_profile',
+        'datetime',
+    )
+    list_display = (
+        'blog',
+        'user_profile',
+        'description',
+        'datetime',
+        )
+    
+    ordering = ('blog',)
+    
+
 
 # Register your models here.
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Blog, Comment
+from .forms import BlogForm, CommentForm
+
 
 # Create your views here.
+
+
+def all_blogs(request):
+    """
+    A view to show all blogs along with search queries
+    """
+    blogs = Blog.objects.all()
+    comments = Comment.objects.all()
+    context = {
+        'blogs': blogs,
+        'comments': comments,
+        'on_blog_page': True,
+    }
+
+    return render(request, 'blogs/blogs.html', context)
